4d3c40985a480795ba60bae98962a2d693f9ffad
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2022-10-03 02:23:51 +0900

    VRM0and1サンプルを追加

---
+++ sample/VRM0and1/SampleBonesReceive.cs
@@ -0,0 +1,200 @@
+﻿/*
+ * SampleBonesReceive (VRM0and1)
+ * gpsnmeajp
+ * https://sh-akira.github.io/VirtualMotionCaptureProtocol/
+ *
+ * These codes are licensed under CC0.
+ * http://creativecommons.org/publicdomain/zero/1.0/deed.ja
+ */
+
+// This is minimum sample of VMCProtocl.
+// Recommend to use EVMC4U.
+
+//これはVMCProtcolの最低限のサンプルです。
+//特段の理由がない場合、EVMC4Uを利用することをおすすめします。
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniVRM10;
+using VRM;
+
+[RequireComponent(typeof(uOSC.uOscServer))]
+public class SampleBonesReceive : MonoBehaviour
+{
+    public GameObject Model;
+    private GameObject OldModel = null;
+
+    Animator animator = null;
+    Vrm10Instance vrm10Root = null;
+    VRMBlendShapeProxy blendShapeProxy = null;
+
+    uOSC.uOscServer server;
+
+    Dictionary<ExpressionKey, float> expressions = new Dictionary<ExpressionKey, float>();
+    Dictionary<BlendShapeKey, float> blends = new Dictionary<BlendShapeKey, float>();
+
+    void Start()
+    {
+        server = GetComponent<uOSC.uOscServer>();
+        server.onDataReceived.AddListener(OnDataReceived);
+
+        Application.targetFrameRate = 60; //60fps
+    }
+
+    void Update()
+    {
+        if (vrm10Root == null)
+        {
+            vrm10Root = Model.GetComponent<Vrm10Instance>();
+        }
+        if (blendShapeProxy == null)
+        {
+            blendShapeProxy = Model.GetComponent<VRMBlendShapeProxy>();
+        }
+    }
+
+    void OnDataReceived(uOSC.Message message)
+    {
+        if (message.address == "/VMC/Ext/Root/Pos")
+        {
+            Vector3 pos = new Vector3((float)message.values[1], (float)message.values[2], (float)message.values[3]);
+            Quaternion rot = new Quaternion((float)message.values[4], (float)message.values[5], (float)message.values[6], (float)message.values[7]);
+
+            Model.transform.localPosition = pos;
+            Model.transform.localRotation = rot;
+        }
+
+        else if (message.address == "/VMC/Ext/Bone/Pos")
+        {
+            //Model updated
+            if (Model != null && OldModel != Model)
+            {
+                animator = Model.GetComponent<Animator>();
+                vrm10Root = Model.GetComponent<Vrm10Instance>();
+                blendShapeProxy = Model.GetComponent<VRMBlendShapeProxy>();
+                OldModel = Model;
+            }
+
+            HumanBodyBones bone;
+            if (Enum.TryParse<HumanBodyBones>((string)message.values[0], out bone))
+            {
+                if ((animator != null) && (bone != HumanBodyBones.LastBone))
+                {
+                    Vector3 pos = new Vector3((float)message.values[1], (float)message.values[2], (float)message.values[3]);
+                    Quaternion rot = new Quaternion((float)message.values[4], (float)message.values[5], (float)message.values[6], (float)message.values[7]);
+
+                    var t = animator.GetBoneTransform(bone);
+                    if (t != null)
+                    {
+                        t.localPosition = pos;
+                        t.localRotation = rot;
+                    }
+                }
+            }
+        }
+        else if (message.address == "/VMC/Ext/Blend/Val")
+        {
+            string BlendName = ((string)message.values[0]).ToLower();
+            float BlendValue = (float)message.values[1];
+
+            if (blendShapeProxy)
+            {
+                //Search Expression
+                bool found = false;
+                foreach (var b in blendShapeProxy.GetValues())
+                {
+                    if (b.Key.Preset == BlendShapePreset.Unknown)
+                    {
+                        if (b.Key.Name.ToLower() == BlendName)
+                        {
+                            blends[b.Key] = BlendValue;
+                            found = true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (b.Key.Preset.ToString().ToLower() == BlendName)
+                        {
+                            blends[b.Key] = BlendValue;
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!found)
+                {
+                    Debug.Log("Not found!" + BlendName);
+                }
+            }
+
+            if (vrm10Root)
+            {
+                string expressionName = BlendName;
+
+                //VRM0 Preset -> VRM1 Preset 
+                switch (expressionName)
+                {
+                    case "joy": expressionName = "happy"; break;
+                    case "angry": expressionName = "angry"; break;
+                    case "sorrow": expressionName = "sad"; break;
+                    case "fun": expressionName = "relaxed"; break;
+                    case "a": expressionName = "aa"; break;
+                    case "i": expressionName = "ih"; break;
+                    case "u": expressionName = "ou"; break;
+                    case "e": expressionName = "ee"; break;
+                    case "o": expressionName = "oh"; break;
+                    case "blink_l": expressionName = "blinkleft"; break;
+                    case "blink_r": expressionName = "blinkright"; break;
+                    default: break;
+                }
+
+                //Search Expression
+                bool found = false;
+                foreach (var e in vrm10Root.Runtime.Expression.ExpressionKeys)
+                {
+                    if (e.Preset == ExpressionPreset.custom)
+                    {
+                        if (e.Name.ToLower() == expressionName)
+                        {
+                            expressions[e] = BlendValue;
+                            found = true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (e.Preset.ToString().ToLower() == expressionName)
+                        {
+                            expressions[e] = BlendValue;
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!found)
+                {
+                    Debug.Log("Not found!" + expressionName);
+                }
+            }
+        }
+
+        else if (message.address == "/VMC/Ext/Blend/Apply")
+        {
+            if (blendShapeProxy)
+            {
+                blendShapeProxy.SetValues(blends);
+                blends.Clear();
+            }
+            if (vrm10Root)
+            {
+                vrm10Root.Runtime.Expression.SetWeights(expressions);
+                expressions.Clear();
+            }
+        }
+    }
+}
---
+++ sample/VRM0and1/SampleBonesSendBundle.cs
@@ -0,0 +1,288 @@
+﻿/*
+ * SampleBonesSendBundle (VRM0and1)
+ * gpsnmeajp
+ * https://sh-akira.github.io/VirtualMotionCaptureProtocol/
+ *
+ * These codes are licensed under CC0.
+ * http://creativecommons.org/publicdomain/zero/1.0/deed.ja
+ */
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniVRM10;
+using VRM;
+using uOSC;
+using UniGLTF;
+
+[RequireComponent(typeof(uOSC.uOscClient))]
+public class SampleBonesSendBundle : MonoBehaviour
+{
+    uOSC.uOscClient uClient = null;
+
+    public GameObject Model = null;
+    private GameObject OldModel = null;
+    public bool vrm0styleExpression = true;
+    public string vrmfilepath;
+    public bool RuntimeLoadGUI = true;
+
+    Animator animator = null;
+    Vrm10Instance vrm10Root = null;
+    VRMBlendShapeProxy blendShapeProxy = null;
+
+    SynchronizationContext synchronizationContext;
+
+    public enum VirtualDevice
+    {
+        HMD = 0,
+        Controller = 1,
+        Tracker = 2,
+    }
+
+    void Start()
+    {
+        uClient = GetComponent<uOSC.uOscClient>();
+        synchronizationContext = SynchronizationContext.Current;
+
+        Application.targetFrameRate = 60; //60fps
+    }
+
+    void Update()
+    {
+        //Only model updated
+        if (Model != null && OldModel != Model)
+        {
+            animator = Model.GetComponent<Animator>();
+            vrm10Root = Model.GetComponent<Vrm10Instance>();
+            blendShapeProxy = Model.GetComponent<VRMBlendShapeProxy>();
+            OldModel = Model;
+        }
+
+        if (Model != null && animator != null && uClient != null)
+        {
+            //Root
+            var RootTransform = Model.transform;
+            if (RootTransform != null)
+            {
+                uClient.Send("/VMC/Ext/Root/Pos",
+                    "root",
+                    RootTransform.position.x, RootTransform.position.y, RootTransform.position.z,
+                    RootTransform.rotation.x, RootTransform.rotation.y, RootTransform.rotation.z, RootTransform.rotation.w);
+            }
+
+            //Bones
+            var boneBundle = new Bundle(Timestamp.Now);
+            foreach (HumanBodyBones bone in Enum.GetValues(typeof(HumanBodyBones)))
+            {
+                if (bone != HumanBodyBones.LastBone)
+                {
+                    var Transform = animator.GetBoneTransform(bone);
+                    if (Transform != null)
+                    {
+                        boneBundle.Add(new Message("/VMC/Ext/Bone/Pos",
+                            bone.ToString(),
+                            Transform.localPosition.x, Transform.localPosition.y, Transform.localPosition.z,
+                            Transform.localRotation.x, Transform.localRotation.y, Transform.localRotation.z, Transform.localRotation.w));
+                    }
+                }
+            }
+            uClient.Send(boneBundle);
+
+            //Virtual Tracker send from bone position
+            var trackerBundle = new Bundle(Timestamp.Now);
+            SendBoneTransformForTracker(ref trackerBundle, HumanBodyBones.Head, "Head");
+            SendBoneTransformForTracker(ref trackerBundle, HumanBodyBones.Spine, "Spine");
+            SendBoneTransformForTracker(ref trackerBundle, HumanBodyBones.LeftHand, "LeftHand");
+            SendBoneTransformForTracker(ref trackerBundle, HumanBodyBones.RightHand, "RightHand");
+            SendBoneTransformForTracker(ref trackerBundle, HumanBodyBones.LeftFoot, "LeftFoot");
+            SendBoneTransformForTracker(ref trackerBundle, HumanBodyBones.RightFoot, "RightFoot");
+            uClient.Send(trackerBundle);
+
+            //Expression
+            if (vrm10Root != null)
+            {
+                var blendShapeBundle = new Bundle(Timestamp.Now);
+
+                foreach (var b in vrm10Root.Runtime.Expression.GetWeights())
+                {
+                    string expressionName = b.Key.ToString();
+
+                    if (vrm0styleExpression)
+                    {
+                        //VRM1 Preset -> VRM0 Preset 
+                        switch (expressionName.ToLower())
+                        {
+                            case "happy": expressionName = "Joy"; break;
+                            case "angry": expressionName = "Angry"; break;
+                            case "sad": expressionName = "Sorrow"; break;
+                            case "relaxed": expressionName = "Fun"; break;
+                            case "aa": expressionName = "A"; break;
+                            case "ih": expressionName = "I"; break;
+                            case "ou": expressionName = "U"; break;
+                            case "ee": expressionName = "E"; break;
+                            case "oh": expressionName = "O"; break;
+                            case "blinkleft": expressionName = "Blink_L"; break;
+                            case "blinkright": expressionName = "Blink_R"; break;
+                            default: break;
+                        }
+                    }
+
+                    uClient.Send("/VMC/Ext/Blend/Val",
+                        expressionName,
+                        (float)b.Value
+                        );
+                }
+                blendShapeBundle.Add(new Message("/VMC/Ext/Blend/Apply"));
+                uClient.Send(blendShapeBundle);
+            }
+
+            //BlendShape
+            if (blendShapeProxy != null)
+            {
+                var blendShapeBundle = new Bundle(Timestamp.Now);
+
+                foreach (var b in blendShapeProxy.GetValues())
+                {
+                    blendShapeBundle.Add(new Message("/VMC/Ext/Blend/Val",
+                        b.Key.ToString(),
+                        (float)b.Value
+                        ));
+                }
+                blendShapeBundle.Add(new Message("/VMC/Ext/Blend/Apply"));
+                uClient.Send(blendShapeBundle);
+            }
+
+            //Available
+            uClient.Send("/VMC/Ext/OK", 1);
+        }
+        else
+        {
+            uClient.Send("/VMC/Ext/OK", 0);
+        }
+        uClient.Send("/VMC/Ext/T", Time.time);
+
+        //Load request
+        uClient.Send("/VMC/Ext/VRM", vrmfilepath, "");
+
+    }
+
+    private void OnGUI()
+    {
+        if (RuntimeLoadGUI) {
+            var ButtonStyle = new GUIStyle(GUI.skin.button);
+            ButtonStyle.fontSize = 24;
+            var TextFieldStyle = new GUIStyle(GUI.skin.textField);
+            TextFieldStyle.fontSize = 24;
+            var LabelStyle = new GUIStyle(GUI.skin.label);
+            LabelStyle.fontSize = 24;
+
+            var path = GUILayout.TextField("C:\\default.vrm", TextFieldStyle);
+            if (GUILayout.Button("Load VRM", ButtonStyle)) {
+                LoadVRM(path);
+            }
+            GUILayout.Label("Port:"+uClient.port, LabelStyle);
+
+            if (GUILayout.Button("Happy(Joy) ON", ButtonStyle))
+            {
+                blendShapeProxy?.SetValue(BlendShapePreset.Joy, 1);
+                vrm10Root?.Runtime.Expression.SetWeight(ExpressionKey.Happy, 1);
+            }
+            if (GUILayout.Button("Happy(Joy) OFF", ButtonStyle))
+            {
+                blendShapeProxy?.SetValue(BlendShapePreset.Joy, 0);
+                vrm10Root?.Runtime.Expression.SetWeight(ExpressionKey.Happy, 0);
+            }
+        }
+    }
+
+    void SendBoneTransformForTracker(ref Bundle bundle, HumanBodyBones bone, string DeviceSerial)
+    {
+        var DeviceTransform = animator.GetBoneTransform(bone);
+        if (DeviceTransform != null) {
+            bundle.Add(new Message("/VMC/Ext/Tra/Pos",
+        (string)DeviceSerial,
+        (float)DeviceTransform.position.x,
+        (float)DeviceTransform.position.y,
+        (float)DeviceTransform.position.z,
+        (float)DeviceTransform.rotation.x,
+        (float)DeviceTransform.rotation.y,
+        (float)DeviceTransform.rotation.z,
+        (float)DeviceTransform.rotation.w));
+        }
+    }
+
+    //Load VRM file on runtime
+    public void LoadVRM(string path)
+    {
+        if (Model != null)
+        {
+            Destroy(Model);
+            Model = null;
+        }
+
+        if (File.Exists(path))
+        {
+            vrmfilepath = path;
+            byte[] VRMdataRaw = File.ReadAllBytes(path);
+            LoadVRMFromData(VRMdataRaw);
+        }
+        else
+        {
+            Debug.LogError("File not found: " + path);
+        }
+    }
+
+    //Load VRM data on runtime
+    //You can receive VRM over the network or file or other.
+    public void LoadVRMFromData(byte[] VRMdataRaw)
+    {
+        GlbLowLevelParser glbLowLevelParser = new GlbLowLevelParser(null, VRMdataRaw);
+        GltfData gltfData = glbLowLevelParser.Parse();
+        try
+        {
+            VRMData vrm = new VRMData(gltfData);
+            VRMImporterContext vrmImporter = new VRMImporterContext(vrm);
+
+            synchronizationContext.Post(async (_) =>
+            {
+                RuntimeGltfInstance gltfInstance = await vrmImporter.LoadAsync(new VRMShaders.ImmediateCaller());
+                gltfData.Dispose();
+                vrmImporter.Dispose();
+
+                Model = gltfInstance.Root;
+                Model.transform.parent = this.transform;
+
+                gltfInstance.EnableUpdateWhenOffscreen();
+                gltfInstance.ShowMeshes();
+            }, null);
+
+            return; //VRM0 Loaded
+        }
+        catch (NotVrm0Exception) { 
+            //continue loading
+        }
+
+        Vrm10Data vrm10 = Vrm10Data.Parse(gltfData);
+        GltfData migratedGltfData = null;
+        Vrm10Importer vrm10Importer = new Vrm10Importer(vrm10);
+
+        synchronizationContext.Post(async (_) => {
+            RuntimeGltfInstance gltfInstance = await vrm10Importer.LoadAsync(new VRMShaders.ImmediateCaller());
+            gltfData.Dispose();
+            vrm10Importer.Dispose();
+
+            if (migratedGltfData != null) {
+                migratedGltfData.Dispose();
+            }
+
+            Model = gltfInstance.Root;
+            Model.transform.parent = this.transform;
+
+            gltfInstance.EnableUpdateWhenOffscreen();
+            gltfInstance.ShowMeshes();
+        }, null);
+    }
+}
